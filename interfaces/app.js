b7e401bc603ddbb067863f249952c0422cfe19f1
Nikita Kent <nikita.kent@icloud.com>
2021-09-05 13:33:30 +1200

    Change username

--- interfaces/app.js
+++ interfaces/app.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
     data() {
       return {
-        firstName: 'Nikita',
-        lastName: 'Doe',
-        email: 'john@gmail.com',
+        firstName: 'Wilhelm',
+        lastName: 'II', 
+        email: 'kaiser@gmail.com',
         gender: 'male',
         picture: 'https://randomuser.me/api/portraits/men/10.jpg',
       }
