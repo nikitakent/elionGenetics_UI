c415eaaaf50c15a739fafc0b2d24e19e5a4e5f25
Nikita Kent <nikita.kent@icloud.com>
2021-09-05 13:30:22 +1200

    Add files

---
+++ contracts/notesToCompile.sol
@@ -0,0 +1,7 @@
+# 
+# Brownie has already been installed.
+# The guides to deploy a smart contract are: 
+# https://youtu.be/SqeHUetM99A 
+# https://betterprogramming.pub/part-1-brownie-smart-contracts-framework-for-ethereum-basics-5efc80205413
+
+# Good Luck Girl! <3 
